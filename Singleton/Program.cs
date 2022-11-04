85cb3838cb17ce4fb1c77c4258d8c111a074b89c
Tomasz Tkocz <tkoczt@gmail.com>
2022-11-04 20:47:47 +0100

    Singleton created

---
+++ Singleton/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("You should see the same text or I will have troubles");
+
+            Thread proccess1 = new Thread(() => GetInstanceOfSingleton("Value number one"));
+            Thread proccess2 = new Thread(() => GetInstanceOfSingleton("Value number two"));
+            Thread proccess3 = new Thread(() => GetInstanceOfSingleton("Value number three"));
+            Thread proccess4 = new Thread(() => GetInstanceOfSingleton("Value number four"));
+            Thread proccess5 = new Thread(() => GetInstanceOfSingleton("Value number five"));
+            Thread proccess6 = new Thread(() => GetInstanceOfSingleton("Value number six"));
+
+            proccess1.Start();
+            proccess2.Start();
+            proccess3.Start();
+            proccess4.Start();
+            proccess5.Start();
+            proccess6.Start();
+
+            proccess1.Join();
+            proccess2.Join();
+            proccess3.Join();
+            proccess4.Join();
+            proccess5.Join();
+            proccess6.Join();
+
+        }
+
+        public static void GetInstanceOfSingleton(string value)
+        {
+            var singleton = SingletonClass.GetInstance(value);
+            Console.WriteLine(singleton.singletonValue);
+        }
+    }
+}
---
+++ Singleton/SingletonClass.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Singleton
+{
+    public class SingletonClass
+    {
+        public string singletonValue { get; set; }
+
+        private static SingletonClass _instance;
+
+        private static readonly object _lock = new object();
+        private SingletonClass() { }
+
+        public static SingletonClass GetInstance(string value)
+        {
+            if (_instance == null)
+            {
+                lock (_lock)
+                {
+                    if (_instance==null)
+                    {
+                        _instance = new SingletonClass();
+                        _instance.singletonValue = value;
+                    }
+                }
+            }
+            return _instance;
+        }
+    }
+}
